68200a5d2a0eaec5661c5ec42b00fff26817c47b
KatePartridge11 <kate.partridge11@gmail.com>
2026-04-13 13:33:09 +0200

    Task 16 Part 2 Uploaded

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Note
+
 
 # Create your tests here.
+class NoteModelTest(TestCase):
+    def setUp(self):
+        Note.objects.create(title="Test Database", content="Task 16 Part 2")
+
+    def test_note_content(self):
+        note = Note.objects.get(id=1)
+        self.assertEqual(note.title, "Test Database")
+        self.assertEqual(note.content, "Task 16 Part 2")
+
+
+class NoteViewTest(TestCase):
+    def setUp(self):
+        Note.objects.create(title="Test Note", content="Testing View")
+
+    def test_note_list_view(self):
+        response = self.client.get(reverse("note_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Note")
+
+    def test_detail_view(self):
+        note = Note.objects.get(id=1)
+        response = self.client.get(reverse("note_detail", args=[str(note.id)]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Testing View")
